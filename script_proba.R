b5a77c268361b2728e577c29ca45e66761a3f3b2
Manuel Cruz Garrote <manuels.cruz@urosario.edu.co>
2024-05-03 08:50:57 -0500

    Se inicia la elaboración de la interfaz presentable al usuario mediante el uso de shiny

--- script_proba.R
+++ script_proba.R
@@ -1,3 +1,6 @@
+
+##### FILTRO PARA LA BASE DE DATOS
+
 # Instalación y Descarga de libreria
 if (!require("dplyr")) {
   install.packages("dplyr")
@@ -19,3 +22,52 @@ db_twins <- db_twins %>%
   filter(HRWAGEH != "." & HRWAGEL != ".")
 
 View(db_twins)
+
+
+
+
+
+library(shiny)
+
+# Definir la interfaz de usuario
+ui <- fluidPage(
+  tags$head(
+    tags$style(
+      HTML("
+      .titulo-centrado {
+        text-align: center;
+        font-family: 'Arial', sans-serif;
+        font-weight: bold
+      }
+      .imagen {
+        display: inline-block;
+        width: 50px; /* Ajusta el tamaño de la imagen según tus necesidades */
+        height: auto; /* Esto mantendrá la proporción de la imagen */
+      }
+      .integrantes {
+        text-align: center;
+        font-size: 16px;
+      }
+      ")
+    )
+  ),
+  titlePanel(
+    div(class = "titulo-centrado", 
+        img(src = file.path(dir, "imagen_izquierda.png"), class = "imagen"),
+        "Proyecto Final Probabilidad y Estadistica 1",
+        img(src = file.path(dir, "imagen_derecha.png"), class = "imagen"),
+        div(class = "integrantes", "Integrantes: Diego Buitrago, Manuel Cruz, Mariana Romero")
+    )
+  )
+)
+
+
+# Define server logic
+server <- function(input, output) {
+  output$mensaje <- renderText({
+    "¡Hola desde mi primera aplicación Shiny!"
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
