e0eab609551a91fa2df318d4ec854fddc1323f62
Manuel Cruz Garrote <manuels.cruz@urosario.edu.co>
2024-05-24 09:23:08 -0500

    Se actualiza todo, y se unifican codigos

--- m.R
+++ m.R
@@ -1,13 +1,7 @@
-####### FILTRO PARA LA BASE DE DATOS
+##### FILTRO PARA LA BASE DE DATOS
 # Instalación y Descarga de libreria
 if (!require("dplyr")) {
   install.packages("dplyr")
-}
-library(dplyr)
-
-# Cargar paquete necesario si no está instalado
-if (!require("ggplot2")) {
-  install.packages("ggplot2")
 }
 library(dplyr)
 
@@ -18,28 +12,27 @@ dir <- getwd()
 twins <- read.csv(file.path(dir, "twins.txt"), header = TRUE)
 View(twins)
 
-# Selección de columnas "Twins" 
+# Selección de columnas "Twins"
 # función select de dplyr
-db <- dplyr::select(twins, HRWAGEH, HRWAGEL, EDUCH, EDUCL)
 
+db_twins <- dplyr::select(twins, HRWAGEH, HRWAGEL)
 
 # Exclusión de datos que se encuentran incompletos
-db <- db %>%
+db_twins <- db_twins %>%
   filter(HRWAGEH != "." & HRWAGEL != ".")
-View(db)
 
-# selección de las horas de estudio
-db_twins <- dplyr::select(db, HRWAGEH, HRWAGEL)
 View(db_twins)
 
-db_hr <- dplyr::select(db, EDUCH, EDUCL)
-View(db_hr)
+####### ESTIMACIÓN KERNELL
 
-#######ESTIMACIÓN KERNELL
-
+# Cargar paquete necesario si no está instalado
+if (!require("ggplot2")) {
+  install.packages("ggplot2")
+}
 
 # Cargar los datos
 db_twins <- select(twins, HRWAGEH, HRWAGEL)
+
 
 ####### ANALISIS NUMERICO 
 twins <- read.csv(file.path(dir, "twins.txt"), header = TRUE)
@@ -163,10 +156,12 @@ max(db_twins$HRWAGEH)
 # Calcular el máximo para el gemelo 2 ("HRWAGEL")
 max(db_twins$HRWAGEL)
 
-#######UI PAGINA WEB
+
+####### UI PAGINA WEB
 library(shiny)
 
 # Guardar los resultados de los cálculos en variables
+
 media_twins1 <- round(mu_twins1, digits = 2)
 media_twins2 <- round(mu_twins2, digits = 2)
 mediana_twins1 <- mediana_twins1
@@ -192,7 +187,8 @@ min_twins2 <- round(min(db_twins$HRWAGEL), digits = 2)
 max_twins1 <- round(max(db_twins$HRWAGEH), digits = 2)
 max_twins2 <- round(max(db_twins$HRWAGEL), digits = 2)
 
-######CARGA PARA LA ESTIMACIÓN KERNELL
+####CARGA PARA LA ESTIMACIÓN KERNELL
+
 # Crear nuevas variables numéricas a partir de las columnas existentes en db_twins
 db_twins_numeric <- db_twins %>%
   mutate(HRWAGEH_numeric = as.numeric(HRWAGEH),
@@ -215,7 +211,7 @@ densidad_hrwagel_numeric <- density(db_twins_numeric$HRWAGEL_numeric)
 plot(densidad_hrwagel_numeric, main = "Densidad de Kernel para HRWAGEL_numeric",
      xlab = "HRWAGEL_numeric", ylab = "Densidad")
 
-######POSIBLE DEPLOY DE LOS GRAFICO DE ESTIMACIÓN KERNELL
+######## POSIBLE DEPLOY DE LOS GRAFICO DE ESTIMACIÓN KERNELL
 install.packages("DT")
 library(DT)
 
@@ -226,7 +222,7 @@ ui <- fluidPage(
       HTML("
       body {
         background-color: #b8d0eb;
-        color: #0a1128;
+        color: #000000;
         padding-left: 5%;
         padding-right: 5%;
       }
@@ -235,52 +231,21 @@ ui <- fluidPage(
         font-family: 'Arial', sans-serif;
         font-weight: bold;
         border-radius: 12px;
-        background-color: #034078;
+        background-color: #03045e;
         color: #fefcfb;
         padding: 10px;
         padding-top: 20px;
-        padding-bottom: 30px;
-        margin-gottom: 10px;
       }
       .imagen {
         display: inline-block;
         width: 50px; /* Ajusta el tamaño de la imagen según tus necesidades */
         height: auto; /* Esto mantendrá la proporción de la imagen */
       }
-      .autores {
-        font-size: 22px;
-        font-weight: bold;
-        margin-bottom: 20px;
-        border-radius: 10px;
-      }
       .integrantes {
-        display: flex;
-        flex-wrap: nowrap; /* Evita que las tarjetas se envuelvan a la siguiente línea */
-        justify-content: center; /* Centra las tarjetas horizontalmente */
-        gap: 20px;
-        background-color: #034078;
-        padding: 20px;
-        margin-bottom: 20px;
-        border-radius: 10px;
-      }
-      .tarjeta {
-        border: 1px solid #ccc;
-        border-radius: 8px;
-        box-shadow: 2px 2px 5px rgba(0,0,0,0.1);
-        background-color: #fff;
-        padding: 20px;
-        width: 250px; /* Ancho de cada tarjeta */
-        display: flex;
-        flex-direction: column;
-        align-items: center;
         text-align: center;
-        border-radius: 10px;
-      }
-      .tarjeta img {
-        border-radius: 50%;
-        width: 150px; /* Ancho de la imagen */
-        height: 150px; /* Altura de la imagen */
-        margin-bottom: 10px;
+        font-size: 16px;
+        font-weight: 500;
+        margin-top: 20px;
       }
       .docente {
         text-alig: center;
@@ -310,27 +275,6 @@ ui <- fluidPage(
         font-size: 22px;
         font-weight: bold;
       }
-      .tratamiento_datos {
-        font-size: 16px;
-        margin: 20px;
-      }
-      .tabla-container {
-        display: flex; /* Establece el contenedor como flexbox */
-        justify-content: center; /* Centra los elementos del contenedor */
-        gap: 20px; /* Espacio entre las tablas */
-      }
-      .tabla-info {
-        border-collapse: collapse; /* Fusiona los bordes de las celdas */
-      }
-      .tabla-info th, .tabla-info td {
-        border: 1px solid black; /* Añade bordes a las celdas */
-        padding: 8px; /* Añade espacio interno a las celdas */
-        text-align: center; /* Centra el texto dentro de las celdas */
-      }
-      .cont_datos {
-        font-size: 16px;
-        margin: 20px;
-      }
       .desc_grafica {
         font-size: 16px;
         margin: 20px;
@@ -341,23 +285,14 @@ ui <- fluidPage(
       }
       .linea-horizontal {
         border: none;
-        border-top: 5px solid #034078; /* Color de la línea */
+        border-top: 5px solid #03045e; /* Color de la línea */
         width: 100%; /* Ancho de la línea */
       }
       .seccion-medidas-tc {
-        background-color: #cfdbd5; /* Color de fondo de la nueva sección */
+        background-color: #f6fff8; /* Color de fondo de la nueva sección */
         padding: 20px;
         margin-top: 20px; /* Espacio superior */
         border-radius: 10px; /* Bordes redondeados */
-      }
-      .seccion-estimacion-kernell {
-        text-align: center; /* Centra los elementos dentro de la sección */
-        padding-top: 10px;
-        padding-bottom: 10px;
-      }
-      .boton-container {
-        margin-bottom: 20px; /* Espacio entre los botones y las gráficas */
-        padding-bottom: 20px;
       }
       ")
     ),
@@ -372,60 +307,23 @@ ui <- fluidPage(
       ')
     )
   ),
-  tags$head(
-    tags$script(HTML('
-      $(document).ready(function(){
-        $("#btn_densidad_hrwageh").click(function(){
-          $("#densidad_hrwageh_numeric").show();
-          $("#densidad_hrwagel_numeric").hide();
-        });
-        $("#btn_densidad_hrwagel").click(function(){
-          $("#densidad_hrwagel_numeric").show();
-          $("#densidad_hrwageh_numeric").hide();
-        });
-      });
-    '))
-  ),
   titlePanel(
     div(class = "titulo-centrado", 
         img(src = file.path(dir, "imagen_izquierda.png"), class = "imagen"),
         "Proyecto Final Probabilidad y Estadistica 1",
-        img(src = file.path(dir, "imagen_derecha.png"), class = "imagen")
+        img(src = file.path(dir, "imagen_derecha.png"), class = "imagen"),
+        div(class = "integrantes", "Integrantes: Manuel Cruz Garrote y Mariana Romero Reyes"),
+        div(class = 'docente', 'Docente: Nicolas López López')
     )
   ),
   
   navbarPage(
     "Barra de Navegación", # Título de la página
-    tabPanel("Autores", onClick = "shiny.setInputValue('nav_clicked', 'integrantes')"),
     tabPanel("Contextualización", onClick = "Shiny.setInputValue('nav_clicked', 'introduccion')"),
-    tabPanel("Datos Estadísticos"),
+    tabPanel("Datos Estadísticos", onClick = "Shiny.setInputValue('nav_clicked', 'datos estadísticos')"),
     tabPanel("Estimación Kernell"),
     tabPanel("Análisis de Resultados"),
     tabPanel("Conclusiones")
-  ),
-  div(class = 'autores', 'Autores:'),
-  div(class = 'integrantes', 
-      div(class = 'tarjeta estudiante1',
-          h3('Manuel Cruz G'),
-          img(src = 'est1_img.jpg'), 
-          p('Estudiante MACC'),
-          p('Esgrimista profesional'),
-          p('manuels.cruz@urosario.edu.co')
-      ),
-      div(class = 'tarjeta estudiante2',
-          h3('Mariana Romero R'),
-          img(src = 'est1_img.jpg'), 
-          p('Estudiante MACC'),
-          p('Estudiante Ing. Sis. Energeticos'),
-          p('mariana.lu@urosario.edu.co')
-      ),
-      div(class = 'tarjeta estudiante3',
-          h3('Nicolas López L'),
-          img(src = 'est1_img.jpg'), 
-          p('Docente'),
-          p('Estadístico (BSc & MSc)'),
-          p('nico@urosario.edu.co')
-      ),
   ),
   
   # Div encapsulador de la sección "Contextualización"
@@ -434,7 +332,7 @@ ui <- fluidPage(
       div(class = 'introduccion',
           'Introducción:'
       ),
-      # Nue:vo apartado para añadir texto explicativo
+      # Nuevo apartado para añadir texto explicativo
       div(class = "explicacion",
           "En esta página se presenta el proyecto final para la asignatura 
           Probabilidad y Estadística 1, correspondiente al semestre 2024-1.
@@ -449,128 +347,44 @@ ui <- fluidPage(
       ), 
       div(class = 'sust_problema', 
           'Se cuenta con información de pares de gemelos monocigóticos mayores de 18 años, y se desea contrastar el fenomeno de años de educación, asociados con el ingreso por hora.'
-      ),
+      ), 
       div(class = 'metodologia', 
-          'Metodologia:',
-      ),
-      div(class = 'tratamiento_datos',
-          'Para el tratamiento de datos se importo la base de datos que fue brindada
-          para el desarrollo del analisis. En esta se encontraron ciertas observaciones que pueden ser relevante y tener importancia a lo largo del analisis de resultados:'
-      ),
-      div(class = 'tabla-container',
-          div(class = 'tabla-info',
-              HTML("
-        <table class='tabla-info'>
-          <tr>
-            <th>Registros</th>
-            <th>Variables</th>
-          </tr>
-          <tr>
-            <td>183</td>
-            <td>16</td>
-          </tr>
-        </table>
-      ")
-          ),
-          div(class = 'tabla-info',
-              HTML("
-        <table class='tabla-info'>
-          <tr>
-            <th>Datos completos</th>
-            <th>Datos incompletos</th>
-          </tr>
-          <tr>
-            <td>149</td>
-            <td>34</td>
-          </tr>
-        </table>
-      ")
-          )
-      ),
-      div(class = 'cont_datos', 
-          'Como ultimo paso de la preparación de la base de datos, se eliminan los datos
-          con información faltante, llegando a una dimensión final de 149 datos con 6
-          variables'
+          'Metodología:',
       ),
       div(class = 'desc_grafica', 
-          'Para contar con una claridad y mayor profundidad en el estudio realizado
-          se describio el reporte de manera gráfica mediante el uso del grafico
-          "Estimación Kernell". Para llegar a la mejor comprensión de esta herramienta
-          grafica es necesario entender sus ventaja, desventajas y como leer este gráfico.
-          Ciertos conceptos fueron adquirido a lo largo de la materia "Probabilidad y 
-          Estadistica I" a lo largo del semestre 2024-1'
+          'Para este caso de estudio, se buscara un forma gráfica de representar los datos que fueron recolectados y expuestos, mediante el grafico asociado con la "Estimación Kernel".'
       ),
-      div(class = 'tabla-container',
-          style = 'display: flex; justify-content: center; gap: 20px;',  # Estilo para centrar el contenedor
-          div(class = 'tabla-info',
-              style = 'margin-left: auto; margin-right: auto;',  # Estilo para centrar la tabla
-              HTML("
-          <table class='tabla-info'>
-            <tr>
-              <th>Categoría</th>
-              <th>Descripción</th>
-            </tr>
-            <tr>
-              <td>Ventajas</td>
-              <td>
-                <ul>
-                  <li>Proporciona una representación suave y continua de la distribución de datos.</li>
-                  <li>Permite visualizar la forma y la estructura de la distribución de datos, especialmente en casos de datos multimodales o con formas no normales.</li>
-                  <li>No requiere la especificación de una distribución paramétrica, lo que lo hace útil cuando no se conocen las características exactas de los datos.</li>
-                  <li>Puede ser útil para identificar patrones y anomalías en los datos.</li>
-                </ul>
-              </td>
-            </tr>
-            <tr>
-              <td>Desventajas</td>
-              <td>
-                <ul>
-                  <li>La interpretación puede ser subjetiva y depende en gran medida de la elección del ancho de banda.</li>
-                  <li>Puede ser sensible a la elección del núcleo utilizado para suavizar la distribución.</li>
-                  <li>No proporciona medidas numéricas directas de la distribución, como la media o la varianza.</li>
-                  <li>Puede ser computacionalmente costoso para conjuntos de datos grandes debido al cálculo de densidades en cada punto.</li>
-                </ul>
-              </td>
-            </tr>
-          </table>
-        ")
-          )
-      )
-      ,
       div(class = 'desc_numerica',
-          'Como ultimo paso para el analisis de los datos, se busca dar un reporte numerico
-          para los datos correspondientes al estudio. En este reporte se puede apreciar medidas
-           numericas e centro, dispersión y localización para los gemelos L y H.',
-           hr(class = "linea-horizontal") # Línea horizontal
+          'Para el caso númerico, se busco realizar una comparación entre cada gurpo de gemelos (gemelo 1 vs gemelo 2), en el cual se busca contrastar las siguientes medidas: (CENTRO: Media, Mediana, Moda), (DISPERSIÓN: Rango, Varianza, Desviación estandar, CV), (LOCALIZACIÓN: Q1, Q2, Q3, Minimo, Maximo)',
+          hr(class = "linea-horizontal") # Línea horizontal
       )
   ),
   
-  # Nueva sección para datos estadísticos
+  # Nueva sección para medidas de TC
   div(class = 'seccion-medidas-tc',
       h2(class = "titulo-centrado", "Datos Estadísticos"),
       # Resultados de los cálculos
       DTOutput("statisticsTable")
-      )
-  ,
+  ),
   
   # Nueva sección para estimación kernell
   div(class = 'seccion-estimacion-kernell',
-      h2(class = "titulo-centrado", "Estimación Kernell para ingresos por hora de gemelos H y L"),
-      actionButton("btn_densidad_hrwageh", "Mostrar Gráfica de HRWAGEH"),
-      actionButton("btn_densidad_hrwagel", "Mostrar Gráfica de HRWAGEL"),
+      h2(class = "titulo-centrado", "Estimación Kernell"),
+      # Agregar aquí los gráficos y descripciones correspondientes
       plotOutput("densidad_hrwageh_numeric"),
       plotOutput("densidad_hrwagel_numeric")
   ),
-
+  
   # Nueva sección para análisis de resultados
   div(class = 'seccion-analisis-de-resultados',
-      h2(class = "titulo-centrado", "Análsis de Resultados"),
+      h2(class = "titulo-centrado", "Análisis de Resultados"),
   ),
-
+  
   # Nueva sección para conclusiones
   div(class = 'seccion-conclusiones',
       h2(class = "titulo-centrado", "Conclusiones"),
   ),
+  
 )
 
 # Define server logic
@@ -578,11 +392,13 @@ server <- function(input, output, session) {
   output$mensaje <- renderText({
     "¡Hola desde mi primera aplicación Shiny!"
   })
+  
   observeEvent(input$nav_clicked, {
     scrollToElement(".introduccion")
   })
+  
   output$statisticsTable <- renderDT({
-    datatable(resultados, options = list(pageLenght = 12, autowidth = TRUE))
+    datatable(resultados, options = list(pageLength = 12, autoWidth = TRUE))
   })
   
   # Crear nuevas variables numéricas a partir de las columnas existentes en db_twins
@@ -595,8 +411,8 @@ server <- function(input, output, session) {
   
   # Graficar la densidad de kernel para HRWAGEH_numeric
   output$densidad_hrwageh_numeric <- renderPlot({
-    plot(densidad_hrwageh_numeric, main = "Densidad de Kernel para Gemelo H",
-         xlab = "Ingreso por horas Gemelo H ($USD)", ylab = "Densidad")
+    plot(densidad_hrwageh_numeric, main = "Densidad de Kernel para HRWAGEH_numeric",
+         xlab = "HRWAGEH_numeric", ylab = "Densidad")
   })
   
   # Calcular la densidad de kernel para HRWAGEL_numeric
@@ -604,8 +420,8 @@ server <- function(input, output, session) {
   
   # Graficar la densidad de kernel para HRWAGEL_numeric
   output$densidad_hrwagel_numeric <- renderPlot({
-    plot(densidad_hrwagel_numeric, main = "Densidad de Kernel para Gemelo L",
-         xlab = "Ingreso por horas Gemelo L ($USD)", ylab = "Densidad")
+    plot(densidad_hrwagel_numeric, main = "Densidad de Kernel para HRWAGEL_numeric",
+         xlab = "HRWAGEL_numeric", ylab = "Densidad")
   })
 }
 
